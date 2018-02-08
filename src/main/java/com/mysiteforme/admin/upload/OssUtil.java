8ecf8d4763f3072899dbb1d65d26f1c96ff99e25
wangl1989 <1115784675@qq.com>
2018-02-08 19:41:44 +0800

    文件上传接口

---
+++ src/main/java/com/mysiteforme/admin/upload/OssUtil.java
@@ -0,0 +1,34 @@
+package com.mysiteforme.admin.upload;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by wangl on 2018/2/8.
+ * todo: 阿里云上传
+ */
+public class OssUtil implements UploadUtil {
+    @Override
+    public String upload(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public Boolean delet(String path) {
+        return null;
+    }
+
+    @Override
+    public String uploadNetFile(String url) {
+        return null;
+    }
+
+    @Override
+    public String uploadLocalImg(String localPath) {
+        return null;
+    }
+
+    @Override
+    public String uploadBase64(String base64) {
+        return null;
+    }
+}
