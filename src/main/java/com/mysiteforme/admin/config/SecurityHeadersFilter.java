52091f2856b772a44678ee10c0094165d3afbf36
wangl1989 <1115784675@qq.com>
2025-02-12 03:42:24 +0800

    feat: 增强安全配置，修复XSS漏洞，优化博客文章服务
    - 启用 CSP，移除 X-Frame-Options 限制，保留 XSS 保护。
    - 增强 XssHttpServletRequestWrapper，转义 HTML 标签，过滤危险标签和属性，添加危险标签检测，避免绕过。
    - 优化 BlogArticleServiceImpl，修复文章频道移除，修复部分查询SQL。

--- src/main/java/com/mysiteforme/admin/config/SecurityHeadersFilter.java
+++ src/main/java/com/mysiteforme/admin/config/SecurityHeadersFilter.java
@@ -15,22 +15,22 @@ public class SecurityHeadersFilter implements Filter {
 
         // 设置安全响应头
         // 更开放的 CSP 配置
-//        httpResponse.setHeader("Content-Security-Policy",
-//                "default-src 'self' * data: blob: 'unsafe-inline' 'unsafe-eval'; " +  // 允许所有默认资源
-//                        "script-src 'self' * 'unsafe-inline' 'unsafe-eval'; " +  // 允许所有脚本源
-//                        "style-src 'self' * 'unsafe-inline'; " +  // 允许所有样式源
-//                        "img-src 'self' * data: blob: https: http:; " +  // 允许所有图片源
-//                        "font-src 'self' * data: https:; " +  // 允许所有字体源
-//                        "connect-src 'self' *; " +  // 允许所有API连接
-//                        "frame-src 'self' *; " +  // 允许所有iframe源
-//                        "media-src 'self' *");     // 允许所有媒体源
-//
-//        // 移除 X-Frame-Options 限制
-//        // httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");  // 注释掉或删除这行
-//
-//        // 保留基本的 XSS 保护
-//        httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
-//        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpResponse.setHeader("Content-Security-Policy",
+                "default-src 'self' * data: blob: 'unsafe-inline' 'unsafe-eval'; " +  // 允许所有默认资源
+                        "script-src 'self' * 'unsafe-inline' 'unsafe-eval'; " +  // 允许所有脚本源
+                        "style-src 'self' * 'unsafe-inline'; " +  // 允许所有样式源
+                        "img-src 'self' * data: blob: https: http:; " +  // 允许所有图片源
+                        "font-src 'self' * data: https:; " +  // 允许所有字体源
+                        "connect-src 'self' *; " +  // 允许所有API连接
+                        "frame-src 'self' *; " +  // 允许所有iframe源
+                        "media-src 'self' *");     // 允许所有媒体源
+
+         //移除 X-Frame-Options 限制
+         httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");  // 注释掉或删除这行
+
+         //保留基本的 XSS 保护
+        httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
 
         chain.doFilter(request, response);
     }
