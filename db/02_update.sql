4b8e5d2ff76da2fdbb8b0cddadb3445cb7817a3d
wangl1989 <1115784675@qq.com>
2025-05-22 21:43:37 +0800

    feat(AI): 添加 AI聊天 API 相关数据并优化数据库配置
    - 在数据库中插入 AI 聊天 API权限和接口信息
    -移除 AI 配置中的 HttpClient 创建方法
    - 在 Druid 数据源配置中添加自定义 WebClient 配置

--- db/02_update.sql
+++ db/02_update.sql
@@ -14,3 +14,13 @@ ALTER TABLE sys_user_device MODIFY user_id bigint NULL COMMENT '用户设备ID';
   修改访问日志表，将user_agent字段类型更改为TEXT
  */
 ALTER TABLE analytics_visit_logs MODIFY user_agent TEXT NULL COMMENT '用户浏览器信息';
+
+/**
+  插入AI聊天API数据
+ */
+INSERT INTO `mysiteforme`.`sys_permission` (`id`, `permission_name`, `permission_code`, `permission_type`, `menu_id`, `icon`, `color`, `sort`, `create_by`, `create_date`, `update_by`, `update_date`, `remarks`, `del_flag`) VALUES ('178', 'AI聊天API', 'ai:chat:api', '3', '2', '', NULL, '13', '1', '2025-05-22 07:04:20', '1', '2025-05-22 07:04:20', '', '0');
+INSERT INTO `mysiteforme`.`sys_permission` (`id`, `permission_name`, `permission_code`, `permission_type`, `menu_id`, `icon`, `color`, `sort`, `create_by`, `create_date`, `update_by`, `update_date`, `remarks`, `del_flag`) VALUES ('179', '获取AI聊天历史记录API', 'ai:history:api', '3', '2', '', NULL, '14', '1', '2025-05-22 07:05:05', '1', '2025-05-22 07:05:05', '', '0');
+
+INSERT INTO `mysiteforme`.`sys_permission_api` (`id`, `permission_id`, `api_url`, `http_method`, `common`, `sort`, `create_by`, `create_date`, `update_by`, `update_date`, `remarks`, `del_flag`) VALUES ('94', '178', '/api/admin/ai/chat', 'POST', '1', '13', '1', '2025-05-22 07:04:20', '1', '2025-05-22 07:04:20', '', '0');
+INSERT INTO `mysiteforme`.`sys_permission_api` (`id`, `permission_id`, `api_url`, `http_method`, `common`, `sort`, `create_by`, `create_date`, `update_by`, `update_date`, `remarks`, `del_flag`) VALUES ('95', '179', '/api/admin/ai/getHistoryList', 'GET', '1', '14', '1', '2025-05-22 07:05:05', '1', '2025-05-22 07:05:05', '', '0');
+
--- server-admin/src/main/java/com/mysiteforme/admin/config/DruidDBConfig.java
+++ server-admin/src/main/java/com/mysiteforme/admin/config/DruidDBConfig.java
@@ -10,7 +10,11 @@ package com.mysiteforme.admin.config;
 
 import com.alibaba.druid.support.jakarta.StatViewServlet;
 import com.alibaba.druid.support.jakarta.WebStatFilter;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +24,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.sql.DataSource;
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
 import java.sql.SQLException;
+import java.time.Duration;
 
 
 @Configuration
@@ -84,6 +93,26 @@ public class DruidDBConfig {
         registrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid,/druid/*,/static/**,/upload/*,/register/*");
         registrationBean.addInitParameter("registrationBean","true");
         return registrationBean;
+    }
+
+    @Bean
+    public WebClientCustomizer myWebClientCustomizer() {
+        return builder -> {
+            // 你的自定义配置
+            ConnectionProvider provider = ConnectionProvider.builder("custom")
+                    .maxIdleTime(Duration.ofSeconds(30))
+                    .maxLifeTime(Duration.ofMinutes(5))
+                    .evictInBackground(Duration.ofSeconds(30))
+                    .build();
+            HttpClient httpClient = HttpClient.create(provider)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                    .doOnConnected(connection -> {
+                        connection.addHandlerLast(new ReadTimeoutHandler(30))
+                                .addHandlerLast(new WriteTimeoutHandler(30));
+                    })
+                    .keepAlive(false);
+            builder.clientConnector(new ReactorClientHttpConnector(httpClient));
+        };
     }
 
 
